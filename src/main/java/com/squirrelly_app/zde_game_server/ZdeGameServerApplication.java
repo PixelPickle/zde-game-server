06de1eca213511eaeb1008f853d5e86f102e5deb
KTBates <KTBates@basspro.com>
2025-10-17 15:36:34 -0500

    Initial Commit

---
+++ src/main/java/com/squirrelly_app/zde_game_server/ZdeGameServerApplication.java
@@ -0,0 +1,13 @@
+package com.squirrelly_app.zde_game_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZdeGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZdeGameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squirrelly_app/zde_game_server/ZdeGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squirrelly_app.zde_game_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdeGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
